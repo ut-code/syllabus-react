15b4bdc7b91bc5d8961079516457c583db943423
kosuzu <mirochacha_u_u_1106@yahoo.co.jp>
2024-05-17 22:34:13 +0900

    [Add] comments

--- syllabus-react/src/components/Timetable.tsx
+++ syllabus-react/src/components/Timetable.tsx
@@ -1,56 +1,69 @@
+// Grid要素を用いると、領域を比で分割することができる
 import {Grid} from "@mantine/core"
+import { ReactNode } from "react";
 
-function GetSlotStyle(height:number){
-    return {"borderColor":"gray","borderWidth":"1px", "borderStyle":"solid", "height":height, padding:0, "margin":"auto","alignItems":"center",display:"flex",justifyContent:"center"};
-}
+// 時限(1-6)の列挙
+const periods = [1,2,3,4,5,6];
 
-export function Timetable(){
+// 曜日の列挙
+const weekdays = ["月","火","水","木","金"]
 
-    const periods = [1,2,3,4,5,6];
-    const weekdays = ["月","火","水","木","金"]
+/**
+ * 作成された時間割の閲覧のための要素
+ */
+export function Timetable():JSX.Element{
 
     return(
         <div style={{"borderColor":"gray","borderWidth":"2px", "borderStyle":"solid"}}>
+
+            {/* 曜日の表示 */}
             <Grid columns={5.5} style={{padding:"7px 6px 7px 6px"}} >
-                <Grid.Col span={0.5} style={GetSlotStyle(30)}><div></div></Grid.Col>
-                {weekdays.map(x=><Grid.Col key={x} span={1} style={GetSlotStyle(30)}>{x}</Grid.Col>)}
+
+                {/* 右上の空欄 */}
+                <SlotElement key={""} height={30} span={0.5}/>
+
+                {/* 曜日 */}
+                {weekdays.map(x=><SlotElement key={""} height={30} span={1}>{x}</SlotElement>)}
+
             </Grid>
-            <div >
-            {periods.map(x=><PeriodSlot key={x} n={x}/>)}
-            </div>
+
+            {/* 1-6限のスロット */}
+            {periods.map(x=><PeriodElement key={x} n={x}/>)}
+
+            {/* 集中講義 */}
             <Grid columns={5.5} style={{padding:"7px 6px 7px 6px"}} >
-                <Grid.Col span={0.5} style={GetSlotStyle(100)}><div>集中</div></Grid.Col>
-                <Grid.Col span={5} style={GetSlotStyle(100)}></Grid.Col>
+
+                {/* 「集中」のラベル */}
+                <SlotElement key={"label"} height={100} span={0.5}>集中</SlotElement>
+
+                {/* 登録された集中講義のスロット */}
+                <SlotElement key={""} height={100} span={5}>class name</SlotElement>
             </Grid>
+
         </div>
     )
 }
 
-function PeriodSlot(props:{n:number}){
+/**
+ * 全曜日の1-6限のスロットを作成
+ * [n:引数に時限]
+ */
+function PeriodElement({n}:{n:number}){
     return(
         <Grid columns={5.5} style={{padding:"7px 6px 7px 6px"}}>
-        <PeriodLabel n={props.n} span={0.5}/>
-        {
-            Array.from({ length: 5 }, (_, i) => i + 1).map(x=>{
-                return <Grid.Col key={x} span={1} style={GetSlotStyle(100)}>class name</Grid.Col>
-            })
-        }
-    </Grid>
+
+            {/* 時限のラベル */}
+            <SlotElement key={n.toString()} height={100} span={0.5}>{n}</SlotElement>
+
+            {/* 講義を表示するスロット */}
+            {weekdays.map(x=><SlotElement key={x+n} height={100} span={1}>class name</SlotElement>)}
+        </Grid>
     )
 }
 
-function PeriodLabel(props:{n:number, span:number}){
-    return (
-        <Grid.Col style={GetSlotStyle(100)} span={props.span}>
-            <Grid columns={2} w={"80%"}>
-                <Grid.Col span={1} m={"auto"}>
-                    <div >{props.n}</div>
-                    </Grid.Col>
-                {/* <Grid.Col span={1}>
-                    <div>11:00</div>
-                    <div>12:00</div>
-                </Grid.Col> */}
-            </Grid>
-        </Grid.Col>
-    )
-}
+/**
+ * 講義を表示するスロット
+ * [key:ループで生成する際のkey, height:要素の高さ, span:Gridのspan]
+ */
+const SlotElement=({children, key,height,span}:{children?:ReactNode,key:string, height:number,span:number })=>
+    <Grid.Col key={key} span={span} style={{height:height, borderColor:"gray", borderWidth:"1px", borderStyle:"solid", alignItems:"center", display:"flex", justifyContent:"center"}}>{children}</Grid.Col>
